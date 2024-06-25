ecacf4f8ed504fe82bce460c9c9a76a673ba1d40
Iceeyess <ice_eyes@mail.ru>
2024-06-25 17:33:16 +0300

    models.py has been written.

--- mailing/models.py
+++ mailing/models.py
@@ -15,6 +15,41 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
+        ordering = ('pk', )
+
+
+class Period(models.Model):
+    period = models.CharField(default=False, verbose_name='Период', help_text='Введите период')
+
+    class Meta:
+        verbose_name = 'период'
+        verbose_name_plural = 'периоды'
+
+
+class Status(models.Model):
+    period = models.CharField(default=False, verbose_name='Статус', help_text='Введите статус отправки')
+
+    class Meta:
+        verbose_name = 'статус'
+        verbose_name_plural = 'статусы'
+
+
+class Message(models.Model):
+    topic = models.CharField(max_length=100, verbose_name='Тема сообщения', help_text='Введите тему сообщения')
+    text = models.TextField(verbose_name='Текст сообщения', help_text='Введите текст сообщения')
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
+
 
 class Mailing(models.Model):
-    date_time_first_try = models.DateTimeField()
+    date_time_first_try = models.DateTimeField(verbose_name='Тема сообщения', help_text='Введите тему сообщения')
+    period = models.ForeignKey(Period, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
