f5efe087197cc92b4bda3d6b60a4a3a48d88cef7
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-06-26 02:40:07 +0300

    CLient app is ready

--- clients/urls.py
+++ clients/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .apps import ClientsConfig
-from clients.views import ClientListView, ClientCreateView
+from clients.views import ClientListView, ClientCreateView, ClientUpdateView, ClientDetailView, ClientDeleteView
 
 app_name = ClientsConfig.name
 
 urlpatterns = [
     path('', ClientListView.as_view(), name='client_list'),
     path('create/', ClientCreateView.as_view(), name='client_create'),
-    path('edit/<int:pk>/', ClientCreateView.as_view(), name='client_update'),
+    path('edit/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
+    path('detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .apps import ClientsConfig
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView, TemplateView
 from clients.models import Client
 from django.urls import reverse_lazy
 
@@ -8,17 +8,33 @@ from django.urls import reverse_lazy
 topic_name = ClientsConfig.name
 
 
+# class ClientLink:
+#     extra_context = {'title': topic_name}
+
+
 class ClientListView(ListView):
     model = Client
     paginate_by = 4
+    extra_context = {'title': topic_name}
 
 
 class ClientCreateView(CreateView):
     model = Client
-    fields = ('email', 'full_name', 'comment', )
-    success_url = f'/{topic_name}/'
+    fields = '__all__'
+    success_url = reverse_lazy('clients:client_list')
 
 
 class ClientUpdateView(UpdateView):
     model = Client
-    success_url = reverse_lazy(f'/{topic_name}/')
+    success_url = reverse_lazy(f'{topic_name}/')
+    fields = '__all__'
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    fields = '__all__'
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('clients:client_list')
