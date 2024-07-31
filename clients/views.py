0b222b8bba501e151ec75cead7d87cecad0bf800
Iceeyess <ice_eyes@mail.ru>
2024-07-31 14:48:48 +0300

    for client apps logic for list_view class is added.

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 
 from config.settings import TOPIC_TUPLE
@@ -16,9 +16,13 @@ topic_name = ClientsConfig.name
 class ClientListView(LoginRequiredMixin, ListView):
     model = Client
     paginate_by = 4
-    extra_context = {'topic_name': topic_name,  # Для возврата в меню
-                     'TOPIC_TUPLE': TOPIC_TUPLE
-                     }
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topic_name'] = topic_name  # Для возврата в меню
+        context['TOPIC_TUPLE'] = TOPIC_TUPLE  # Для визуального переключения разделов в шаблоне
+        context['clients_list'] = Client.objects.filter(owner=self.request.user) # список всех клиентов созданных юзером
+        return context
 
 
 class ClientCreateView(LoginRequiredMixin, CreateView):
@@ -37,7 +41,7 @@ class ClientCreateView(LoginRequiredMixin, CreateView):
             return super().form_valid(form)
 
 
-class ClientUpdateView(LoginRequiredMixin, UpdateView):
+class ClientUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Client
     success_url = reverse_lazy('clients:client_list')
     form_class = ClientForm
@@ -45,19 +49,22 @@ class ClientUpdateView(LoginRequiredMixin, UpdateView):
     extra_context = {'topic_name': topic_name,  # Для возврата в меню
                      'TOPIC_TUPLE': TOPIC_TUPLE
                      }
+    permission_required = 'clients.change_client'
 
 
-class ClientDetailView(LoginRequiredMixin, DetailView):
+class ClientDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Client
     form_class = ClientForm
     extra_context = {'topic_name': topic_name,  # Для возврата в меню
                      'TOPIC_TUPLE': TOPIC_TUPLE
                      }
+    permission_required = 'clients.view_client'
 
 
-class ClientDeleteView(LoginRequiredMixin, DeleteView):
+class ClientDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Client
     success_url = reverse_lazy('clients:client_list')
     extra_context = {'topic_name': topic_name,  # Для возврата в меню
                      'TOPIC_TUPLE': TOPIC_TUPLE
                      }
+    permission_required = 'clients.delete_client'
